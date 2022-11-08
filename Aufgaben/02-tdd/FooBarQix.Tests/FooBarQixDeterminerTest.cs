bc88541300a92bb2a782e12932ff4095b9c69b7c
Y <Yannic_Studer1@sluz.ch>
2022-11-08 23:44:13 +0100

    ctrl c/ ctrl v

--- Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
+++ Aufgaben/02-tdd/FooBarQix.Tests/FooBarQixDeterminerTest.cs
@@ -4,9 +4,124 @@ namespace FooBarQix.Tests
 {
     public class FooBarQixDeterminerTest
     {
-        [Fact]
-        public void Test1()
+        public void generalTest()
         {
+            //Arrange
+            double num = 1;
+            string expected = "1";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            //Act
+            string actual = fooBarQixDeterminer.determine(num);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void fooTest()
+        {
+            //Arrange
+            double num = 3;
+            string expected = "foo";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            //Act
+            string actual = fooBarQixDeterminer.determine(num);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void barTest()
+        {
+            //Arrange
+            double num = 5;
+            string expected = "bar";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            //Act
+            string actual = fooBarQixDeterminer.determine(num);
+
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void qixTest()
+        {
+            //Arrange
+            double num = 7;
+            string expected = "qix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            //Act
+            string actual = fooBarQixDeterminer.determine(num);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void fooQixTest()
+        {
+            //Arrange
+            double num = 21;
+            string expected = "fooqix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            //Act
+            string actual = fooBarQixDeterminer.determine(num);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void fooBarTest()
+        {
+            //Arrange
+            double num = 15;
+            string expected = "foobar";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            //Act
+            string actual = fooBarQixDeterminer.determine(num);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void barQixTest()
+        {
+            //Arrange
+            double num = 35;
+            string expected = "barqix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            //Act
+            string actual = fooBarQixDeterminer.determine(num);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void fooBarQixTest()
+        {
+            //Arrange
+            double num = 105;
+            string expected = "foobarqix";
+            FooBarQixDeterminer fooBarQixDeterminer = new FooBarQixDeterminer();
+
+            //Act
+            string actual = fooBarQixDeterminer.determine(num);
+
+            //Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
--- Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
+++ Aufgaben/02-tdd/FooBarQix/FooBarQixDeterminer.cs
@@ -2,6 +2,26 @@
 {
     public class FooBarQixDeterminer
     {
-
+        public string determine(double param)
+        {
+            string res = "";
+            if ((param % 3) == 0)
+            {
+                res += "foo";
+            }
+            if ((param % 5) == 0)
+            {
+                res += "bar";
+            }
+            if ((param % 7) == 0)
+            {
+                res += "qix";
+            }
+            if (res == "")
+            {
+                res = param.ToString();
+            }
+            return res;
+        }
     }
 }
